029e8e20cb752dbb4a6dc5fc92b740c9080d7134
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-07-08 17:00:22 +0330

    start the logic for submit or deny the agent or tretury

--- src/components/ActionByRole.tsx
+++ src/components/ActionByRole.tsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
+
 type Props = {
   userRole: string | "agent" | "master";
 };
 
 function ActionByRole({ userRole }: Props) {
+  const [isTruOpen, setIsTruOpen] =
+    useState<React.SetStateAction<boolean>>(false);
   return (
     <>
-      {" "}
       {userRole === "agent" && (
         <button
           className="bg-purple-600 text-xs px-3 py-1.5 rounded-md font-semibold text-white  hover:bg-purple-800"
-          type="submit"
+          type="button"
         >
           اعلان وضعیت کارشناس
         </button>
       )}
-      {userRole === "master" && (
-        <button className="bg-orange-600 text-xs px-3 py-1.5 rounded-md font-semibold text-white  hover:bg-orange-800">
+      {(userRole === "master" || 1 < 23) && (
+        <button
+          onClick={() => setIsTruOpen(true)}
+          type="button"
+          className="bg-orange-600 text-xs px-3 py-1.5 rounded-md font-semibold text-white  hover:bg-orange-800 relative"
+        >
           اعلان وضعیت خزانه
         </button>
+      )}
+
+      {isTruOpen && (
+        <div
+          className="min-w-40 min-h-40
+      rounded-lg bg-black fixed mx-auto "
+        >
+          hi
+          <span onClick={() => setIsTruOpen(false)} className="text-white">
+            *
+          </span>
+        </div>
       )}
     </>
   );
