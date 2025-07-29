688369487a035c0f8fb04b5c0334f0116233cd11
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-07-29 17:19:09 +0330

    ui optimization and shown sayadiconfirmdata

--- src/components/SayadiConfirmModal.tsx
+++ src/components/SayadiConfirmModal.tsx
@@ -12,6 +12,58 @@ type SayadHolders = {
   name: string;
 };
 
+const reasonMap: Record<string, string> = {
+  POSA: "واریز حقوق",
+  IOSP: "امور بیمه خدمات",
+  HIPA: "امور درمانی",
+  ISAP: "امور سرمایه گذاری و بورس",
+  FXAP: "امور ارزی در چارچوب ضوابط و مقررات",
+  DRPA: "پرداخت قرض و تادیه دیون",
+  RTAP: "امور بازنشستگی",
+  MPTP: "معاملات اموال منقول",
+  IMPT: "معاملات اموال غیر منقول",
+  LMAP: "مدیریت نقدینگی",
+  CDAP: "عوارض گمرکی",
+  TCAP: "تسویه مالیاتی",
+  GEAC: "سایر خدمات دولتی",
+  LRPA: "تسهیلات و تعهدات",
+  CCPA: "تودیع وثیقه",
+  GPAC: "هزینه عمومی و امور روزمره",
+  CPAC: "کمک‌های خیریه",
+  GPPC: "خرید کالا",
+  SPAC: "خرید خدمات",
+};
+
+function convertSayadConfirmReasonToMessage(code: string): string {
+  return reasonMap[code] ?? "نامشخص";
+}
+
+const chequeStatusMap: Record<string, string> = {
+  "1": "صادر شده",
+  "2": "نقد شده",
+  "3": "باطل شده",
+  "4": "برگشت خورده",
+  "5": "بخشی برگشت خورده",
+  "6": "در انتظار امضای ضامن",
+  "7": "در انتظار تایید گیرنده در کشیدن چک",
+  "8": "در انتظار تایید گیرنده در انتقال چک",
+};
+
+const blockStatusMap: Record<string, string> = {
+  "0": "  چک مسدود نشده است",
+  "1": "  مسدود موقت می‌باشد",
+  "2": "  مسدود دائم می‌باشد",
+  "3": "  چک رفع مسدودی شده است",
+};
+
+function convertChequeStatusToMessage(code: string): string {
+  return chequeStatusMap[code] ?? "وضعیت نامشخص";
+}
+
+function convertBlockStatusToMessage(code: string): string {
+  return blockStatusMap[code] ?? "وضعیت نامشخص";
+}
+
 function SayadiConfirmModal({ data, closeModal }: Props) {
   const [sayadConfirmHoldersArray, setSayadConfirmHoldersArray] = useState<
     SayadHolders[]
@@ -49,6 +101,21 @@ function SayadiConfirmModal({ data, closeModal }: Props) {
             </div>
           </div>
         ))}
+        <div>
+          {data.sayadConfirmReason &&
+            convertSayadConfirmReasonToMessage(data.sayadConfirmReason)}
+        </div>
+
+        <div>
+          {data.sayadConfirmBlockStatus &&
+            convertBlockStatusToMessage(data.sayadConfirmBlockStatus)}
+        </div>
+        <div>
+          {data.sayadConfirmChequeStatus &&
+            convertChequeStatusToMessage(data.sayadConfirmChequeStatus)}
+        </div>
+        <div>{data.sayadConfirmChequeType}</div>
+        <div>{data.sayadConfirmGuaranteeStatus}</div>
         <button
           className="bg-sky-500 px-1.5 py-1 rounded-md cursor-pointer text-white hover:bg-white hover:text-sky-500"
           onClick={() => closeModal()}
